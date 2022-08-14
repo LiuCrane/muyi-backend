90500f2acd7df0a36adb7b41e29f7f4f16a202b1
Ivan <sbangm@foxmail.com>
2022-08-14 12:53:13 +0800

    S2: App 门店加盟商接口定义

--- src/main/java/com/mysl/api/controller/app/StoreController.java
+++ src/main/java/com/mysl/api/controller/app/StoreController.java
@@ -4,6 +4,7 @@ import com.mysl.api.common.lang.ResponseData;
 import com.mysl.api.config.security.JwtTokenUtil;
 import com.mysl.api.entity.Store;
 import com.mysl.api.entity.dto.StoreDTO;
+import com.mysl.api.entity.dto.StoreSimpleDTO;
 import com.mysl.api.service.StoreService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @author Ivan Su
  * @date 2022/8/4
@@ -21,18 +24,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/app/store")
 @Slf4j
-@Secured({"ROLE_APP_USER", "ROLE_STORE_MANAGER"})
 public class StoreController {
 
     @Autowired
     StoreService storeService;
 
     @ApiOperation("查询门店信息")
+    @Secured({"ROLE_APP_USER", "ROLE_STORE_MANAGER"})
     @GetMapping
     public ResponseData<StoreDTO> get() {
         StoreDTO dto = new StoreDTO();
         Store store = storeService.findByUserId(JwtTokenUtil.getCurrentUserId());
         BeanUtils.copyProperties(store, dto);
         return ResponseData.ok(dto);
+    }
+
+    @ApiOperation("查询门店加盟商列表")
+    @Secured({"ROLE_STORE_MANAGER"})
+    @GetMapping("/franchisees")
+    public ResponseData<List<StoreSimpleDTO>> getFranchisees(@RequestParam(defaultValue = "0", required = false) Integer offset,
+                                                             @RequestParam(defaultValue = "20", required = false) Integer limit){
+        return ResponseData.ok();
     }
 }
---
+++ src/main/java/com/mysl/api/entity/dto/StoreSimpleDTO.java
@@ -0,0 +1,40 @@
+package com.mysl.api.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mysl.api.entity.enums.StoreStatus;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Su
+ * @date 2022/8/5
+ */
+@ApiModel("门店信息")
+@Data
+public class StoreSimpleDTO implements Serializable {
+
+    private static final long serialVersionUID = 7827903987606496439L;
+
+    @ApiModelProperty(value = "门店名称")
+    private String name;
+
+    @ApiModelProperty("门店编号")
+    private String number;
+
+    @ApiModelProperty("店长")
+    @JsonProperty("manager_name")
+    private String managerName;
+
+    @ApiModelProperty(value = "门店地址")
+    private String address;
+
+    @ApiModelProperty(value = "门店位置纬度")
+    private String lat;
+
+    @ApiModelProperty(value = "门店位置经度")
+    private String lng;
+
+}
