ce7442ba5c8c59f55ba3ef01ae2a31c4db6a0744
Ivan <sbangm@foxmail.com>
2022-08-27 17:36:46 +0800

    S2: 新增后台直传COS所需信息

--- src/main/java/com/mysl/api/common/util/CosUtil.java
+++ src/main/java/com/mysl/api/common/util/CosUtil.java
@@ -1,5 +1,7 @@
 package com.mysl.api.common.util;
 
+import com.mysl.api.entity.dto.UploadInfo;
+import com.mysl.api.entity.enums.FileType;
 import com.qcloud.cos.COSClient;
 import com.qcloud.cos.ClientConfig;
 import com.qcloud.cos.auth.BasicCOSCredentials;
@@ -8,14 +10,21 @@ import com.qcloud.cos.http.HttpMethodName;
 import com.qcloud.cos.http.HttpProtocol;
 import com.qcloud.cos.model.GeneratePresignedUrlRequest;
 import com.qcloud.cos.region.Region;
+import com.tencent.cloud.CosStsClient;
+import com.tencent.cloud.Credentials;
+import com.tencent.cloud.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.velocity.shaded.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URL;
-import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Tecent Cloud COS Client Util
@@ -66,6 +75,8 @@ public class CosUtil {
         this.bucketName = bucketName;
     }
 
+    private static final Map<FileType, String> DIR_MAP = Map.of(FileType.IMAGE, "img/", FileType.AUDIO, "aud/", FileType.VIDEO, "vid/");
+
     private static COSClient getCosClient() {
         if (null == cosClient) {
             synchronized (CosUtil.class) {
@@ -84,6 +95,52 @@ public class CosUtil {
         return cosClient;
     }
 
+    private static Credentials getCredential(String allowPrefix) {
+        TreeMap<String, Object> config = new TreeMap<String, Object>();
+        try {
+            // 云 api 密钥 SecretId
+            config.put("secretId", secretId);
+            // 云 api 密钥 SecretKey
+            config.put("secretKey", secretKey);
+
+            // 设置域名
+            //config.put("host", "sts.internal.tencentcloudapi.com");
+
+            // 临时密钥有效时长，单位是秒
+            config.put("durationSeconds", 1800);
+
+            // 换成你的 bucket
+            config.put("bucket", bucketName);
+            // 换成 bucket 所在地区
+            config.put("region", region);
+
+            // 可以通过 allowPrefixes 指定前缀数组, 例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+            config.put("allowPrefixes", new String[] {
+                    allowPrefix
+            });
+
+            // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+            String[] allowActions = new String[] {
+                    // 简单上传
+                    "name/cos:PutObject",
+                    "name/cos:PostObject",
+                    // 分片上传
+                    "name/cos:InitiateMultipartUpload",
+                    "name/cos:ListMultipartUploads",
+                    "name/cos:ListParts",
+                    "name/cos:UploadPart",
+                    "name/cos:CompleteMultipartUpload"
+            };
+            config.put("allowActions", allowActions);
+
+            Response response = CosStsClient.getCredential(config);
+            return response.credentials;
+        } catch (Exception e) {
+            log.error("CosUtil getCredential error: ", e);
+            throw new IllegalArgumentException("no valid secret !");
+        }
+    }
+
     // 获取下载的预签名连接
     public static String generateSimplePresignedDownloadUrl(String filePath, Date expirationDate) {
         GeneratePresignedUrlRequest req =
@@ -93,16 +150,32 @@ public class CosUtil {
         req.setExpiration(expirationDate);
 
         URL url = getCosClient().generatePresignedUrl(req);
-
-        return UriComponentsBuilder.fromUriString(url.toString()).queryParam("ci-process", "pm3u8").queryParam("expires", 3600).build().toString();
-
-    }
-
-    // 生成预签名的上传连接
-    public static String generatePresignedUploadUrl(String key) {
-        Date expirationTime = new Date(System.currentTimeMillis() + 5 * 60 * 1000);
-        URL url = getCosClient().generatePresignedUrl(bucketName, key, expirationTime, HttpMethodName.PUT);
         return url.toString();
+//        return UriComponentsBuilder.fromUriString(url.toString()).queryParam("ci-process", "pm3u8").queryParam("expires", 3600).build().toString();
+
     }
+
+    // 生成包含预签名上传连接的信息
+    public static UploadInfo generatePresignedUploadUrl(FileType fileType, String filename) {
+        Date expirationTime = new Date(System.currentTimeMillis() + 20 * 60 * 1000);
+//        URL url = getCosClient().generatePresignedUrl(bucketName, key, expirationTime, HttpMethodName.PUT);
+//        return url.toString();
+        HttpMethodName method = HttpMethodName.PUT;
+        String dir = DIR_MAP.get(fileType);
+        String ext = FilenameUtils.getExtension(filename);
+        filename = DigestUtils.md5DigestAsHex(String.format("%s%s", filename, System.currentTimeMillis()).getBytes(StandardCharsets.UTF_8));
+        String key = String.format("%s%s.%s", dir, filename, ext);
+        Credentials credentials = getCredential(String.format("%s*", dir));
+        COSCredentials cred = new BasicCOSCredentials(credentials.tmpSecretId, credentials.tmpSecretKey);
+        ClientConfig clientConfig = new ClientConfig(new Region(region));
+        COSClient client = new COSClient(cred, clientConfig);
+        URL url = client.generatePresignedUrl(bucketName, key, expirationTime, method);
+        return UploadInfo.builder()
+                .endpoint(endpoint)
+                .url(UriComponentsBuilder.fromUriString(url.toString())
+                        .queryParam("x-cos-security-token", credentials.sessionToken).build().toString())
+                .key(key).method(method.name()).build();
+    }
+
 
 }
