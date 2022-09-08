5e57b479354adb39c1e521362fc5e00924417ff9
Ivan <sbangm@foxmail.com>
2022-09-08 15:52:27 +0800

    S2: fixed bug

--- src/main/java/com/mysl/api/entity/dto/ApplicationDTO.java
+++ src/main/java/com/mysl/api/entity/dto/ApplicationDTO.java
@@ -2,6 +2,7 @@ package com.mysl.api.entity.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,11 +24,18 @@ import java.util.Date;
 public class ApplicationDTO implements Serializable {
     private static final long serialVersionUID = -7957961785562850574L;
 
+    @ApiModelProperty("申请id")
     private Long id;
+    @ApiModelProperty("店长姓名")
     private String storeManager;
+    @ApiModelProperty("门店名称")
     private String storeName;
+    @ApiModelProperty("店长手机号")
     private String storeManagerPhone;
+    @ApiModelProperty("地址")
     private String storeAddress;
+    @ApiModelProperty("申请阶段")
     private String stage;
+    @ApiModelProperty("申请时间")
     private Date createdAt;
 }
